5cffc02a76dfd8feb68c6a842a0486a76efac2c7
Mina Merzeq <minamerzekluffy@gmail.com>
2021-01-25 02:12:15 +0200

    Notes-and-bills

---
+++ Lawyer/Proxy/Archive.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lawyer.Proxy
+{
+    /// <summary>
+    /// Interaction logic for Archive.xaml
+    /// </summary>
+    public partial class Archive : Page
+    {
+        public Archive()
+        {
+            InitializeComponent();
+        }
+
+        private void AddBillBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RemoveBillBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SearchBillsTxt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void GridView_Bills_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void AddNoteBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RemoveNoteBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SearchNotesTxt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void GridView_Notes_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
