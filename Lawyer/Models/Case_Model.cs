ca9bbb22feb6c9f3661e5c0552e50fafa176acf5
Bebo <abanobemad>
2021-02-06 16:44:29 +0200

    24oper

---
+++ Lawyer/Models/Case_Model.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lawyer.Models
+{
+    class Case_Model
+    {
+        public long ID_Case { get; set; }
+        public string Client_Name { get; set; }
+        public string Type_Case { get; set; }
+        public string Notes { get; set; }
+        public string Lock { get; set; }
+    }
+}
